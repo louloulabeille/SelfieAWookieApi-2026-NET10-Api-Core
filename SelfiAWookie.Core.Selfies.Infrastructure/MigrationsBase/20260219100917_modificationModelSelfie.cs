ecf4b1457d19bf99d59b41c7f85ef436e1b70f0a
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-02-19 11:13:38 +0100

    la modification dans la base était déjà faite implicitement - comme je l'avais lié ave l'object

---
+++ SelfiAWookie.Core.Selfies.Infrastructure/MigrationsBase/20260219100917_modificationModelSelfie.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SelfieAWookie.Core.Selfies.Infrastructure.MigrationsBase
+{
+    /// <inheritdoc />
+    public partial class modificationModelSelfie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
