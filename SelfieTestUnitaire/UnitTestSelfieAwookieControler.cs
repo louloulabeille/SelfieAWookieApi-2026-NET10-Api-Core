6724d483cc3aa67818abe003a7240ab9313672a0
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-02-19 16:53:17 +0100

    Mise en place de Des Test Unitaires avec mise en place de entity InMemory

---
+++ SelfieTestUnitaire/UnitTestSelfieAwookieControler.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SelfieAWookie.Core.Selfies.Infrastructure;
+using SelfieAWookie.Core.Selfies.Infrastructure.Repository;
+using SelfieAWookie.Core.Selfies.Interface.Repository;
+using SelfieAWookieApi.Controllers;
+using SelfieAWookies.Selfies.Domain;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SelfieTestUnitaire
+{
+    // création d'une classe de contexte de base de données en mémoire pour les tests unitaires
+    // cette classe hérite de la classe de contexte de base de données réelle
+    // et utilise la méthode OnConfiguring pour configurer une base de données en mémoire
+    public class  SelfieContextMemory : SelfieAWookieDbContext
+    {
+       protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+       {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "Selfie-Dev");
+       }
+    }
+
+    public class UnitTestSelfieAwookieControler
+    {
+        #region private fields
+        // context InMemorry pour les tests unitaires
+        private readonly SelfieContextMemory _context;
+        #endregion
+
+        #region constructeur
+        public UnitTestSelfieAwookieControler()
+        {
+            // initialisation du contexte de base de données en mémoire et ajout de données de test
+            _context = new SelfieContextMemory();
+            _context.Selfies.Add(new Selfie
+            {
+                Id = 1,
+                Title = "Selfie 1",
+                ImagePath = "path/to/image1.jpg",
+                WookieId = 1,
+                Wookie = new Wookie
+                {
+                    Id = 1,
+                    Name = "Wookie 1"
+                }
+            });
+            _context.Selfies.Add(new Selfie
+            {
+                Id = 2,
+                Title = "Selfie 2",
+                ImagePath = "path/to/image2.jpg",
+                WookieId = 2,
+                Wookie = new Wookie
+                {
+                    Id = 2,
+                    Name = "Wookie 2"
+                }
+            });
+            _context.SaveChanges();
+        }
+        #endregion
+
+        [Fact]
+        public void ShouldReturnGetAllSelfie()
+        {
+            //Arrange
+
+            /* utilisation du mock mais il faut plmutot utiliser inMemory */
+            /*
+            var mockRepository = new Mock<ISelfieRepository>();
+            mockRepository.Setup(x => x.GetAll()).Returns(new List<Selfie>
+            {
+                new Selfie
+                {
+                    Id = 1,
+                    Title = "Selfie 1",
+                    ImagePath = "path/to/image1.jpg",
+                    WookieId = 1,
+                    Wookie = new Wookie
+                    {
+                        Id = 1,
+                        Name = "Wookie 1"
+                    }
+                },
+                new Selfie
+                {
+                    Id = 2,
+                    Title = "Selfie 2",
+                    ImagePath = "path/to/image2.jpg",
+                    WookieId = 2,
+                    Wookie = new Wookie
+                    {
+                        Id = 2,
+                        Name = "Wookie 2"
+                    }
+                }
+            });
+            */
+            ISelfieRepository repository = new SelfieDepository(_context);
+
+            //Données à retourner
+            var controller = new SelfieAWookieController(repository);
+
+            //Act
+            var result = controller.GetAll();
+            var okResult = result as OkObjectResult; // Cast du résultat en OkObjectResult
+            IEnumerable<Selfie>? selfies = okResult!.Value as IEnumerable<Selfie>; // Cast de la valeur du résultat en IEnumerable<Selfie>
+
+            //Assert
+            Assert.NotNull(selfies); // Vérifie que le résultat n'est pas null
+            Assert.True(selfies.Any()); // Vérifie que la collection contient au moins un élément
+            Assert.IsType<Selfie> (selfies.First()); // Vérifie que le type des éléments de la collection est Selfie
+        }
+    }
+}
