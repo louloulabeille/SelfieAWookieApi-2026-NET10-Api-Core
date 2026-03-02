e160f2926c7bb11e6f760f8c558949db3a9d4829
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-03-02 13:05:34 +0100

    Mise en place du controller pour l'enregistrement et la mise en placed du tokken

---
+++ SelfieAWookieApi/Applications/DTO/AuthDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfieAWookieApi.Applications.DTO
+{
+    public class AuthDTO
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ SelfieAWookieApi/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SelfieAWookieApi.Applications.DTO;
+
+namespace SelfieAWookieApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public IActionResult SaveAuth([FromBody] AuthDTO auth)
+        {
+            if(auth is not null || (string.IsNullOrEmpty(auth?.Login) && string.IsNullOrEmpty(auth?.Password))) return this.BadRequest("Problème avec l'enregistrement de votre compte");
+
+            
+
+            return this.Ok();
+        }
+    }
+}
