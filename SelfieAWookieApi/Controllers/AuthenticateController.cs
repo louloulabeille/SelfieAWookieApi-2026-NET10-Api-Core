695df4c8a6b5507ec82d46d1cc80078a0d8a4f79
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-03-03 13:57:51 +0100

    mise en place du login et envoi d'un tokken pour garder la connexion.

--- SelfieAWookieApi/Controllers/AuthenticateController.cs
+++ SelfieAWookieApi/Controllers/AuthenticateController.cs
@@ -10,11 +10,12 @@ namespace SelfieAWookieApi.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
-    public class AuthenticateController(UserManager<IdentityUser> userManager, IConfiguration configuration) : ControllerBase
+    public class AuthenticateController( IConfiguration configuration, SignInManager<IdentityUser> signInManager) : ControllerBase
     {
         #region private fields
-        private readonly UserManager<IdentityUser> _userManager = userManager;
+        //private readonly UserManager<IdentityUser> _userManager = userManager;
         private readonly IConfiguration _configuration = configuration;
+        private readonly SignInManager<IdentityUser> _signInManager = signInManager;
         #endregion
 
         [Route("register")]
@@ -35,7 +36,7 @@ namespace SelfieAWookieApi.Controllers
             };
 
             // - mettre le mot de passe en paramètre pour qu'il le hash et le vérifie selon le paramétrage du mot de passe
-            var succes = await _userManager.CreateAsync(user,auth.Password);
+            var succes = await _signInManager.UserManager.CreateAsync(user,auth.Password);
 
             if (succes.Succeeded) { 
                 auth.Password = string.Empty;
@@ -52,17 +53,38 @@ namespace SelfieAWookieApi.Controllers
         {
             if ( string.IsNullOrEmpty(auth.Login) ||  string.IsNullOrEmpty(auth.Password) ) this.BadRequest("Authentication impossible.");
 
+            /* 
+             var user = await _userManager.FindByEmailAsync(auth.Login);
+
+             if(user is not null && await _userManager.CheckPasswordAsync(user, auth.Password!))
+             {
+                 auth.Password = string.Empty;
+                 auth.Token = SecurityTokenGenerate.GenerateTokenJWT(_configuration, user);
+                 return this.Ok(auth);
+             }*/
+
+            // -- Recherche du compte user dans la base de données
+            var user    = await _signInManager.UserManager.FindByEmailAsync(auth.Login);
             
-            var user = await _userManager.FindByEmailAsync(auth.Login);
-
-            if(user is not null && await _userManager.CheckPasswordAsync(user, auth.Password!))
+            if (user is not null)
             {
-                auth.Password = string.Empty;
-                auth.Token = SecurityTokenGenerate.GenerateTokenJWT(_configuration, user);
-                return this.Ok(auth);
-            }
+                // -- comparaison entre les mots de passe et mise en activé du système de bloquer le compte si le nombre de 
+                // -- tentative est supérieur de 3
+                var result = await _signInManager.CheckPasswordSignInAsync(user, auth.Password!, true);
 
-            return this.BadRequest("Problem with password or loggin.");
+                if (result.Succeeded) { // -- ré&sultat positive
+                    // password ok
+                    auth.Password = string.Empty;
+                    auth.Token = SecurityTokenGenerate.GenerateTokenJWT(_configuration, user);
+                    return this.Ok(auth);
+                }
+                 if ( result.IsLockedOut ) // -- compte locked si tentative > 3
+                {
+                    return this.BadRequest("Problem, your account is locked out 10 minutes.");
+                }
+                   
+            }
+            return this.BadRequest("Invalid password or loggin. Good luck.");
         }
     }
 }
